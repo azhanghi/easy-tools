4873984aa40023b22dce092d417c05e0935e2bf2
azhanghi <chaochun0115@gmail.com>
2025-04-26 22:22:51 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
     export function areaOfRectangle(X: number, Y:number): number {
-        led.plot(X, Y)
+        return X*Y
     }
 }
