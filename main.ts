432e7c6f37884470acba193103256ef7090ccff2
azhanghi <chaochun0115@gmail.com>
2025-04-26 23:54:56 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
     /**
-    * 計算長方形面積，並回傳
+    * 計算X * Y
     */
     //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
     //% blockGap=2 weight=0 blockExternalInputs=true
