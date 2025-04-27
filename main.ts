510def33e85b732f2559b9a56818ac7ca2c708ac
azhanghi <chaochun0115@gmail.com>
2025-04-27 11:02:46 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,19 +1,100 @@
-//% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
-namespace tools {
-    /**
-    * 計算X * Y
+// 在這裡添加你的程式
+//% weight=0 color=#3CB371 icon="\uf2db" block="gigotools" groups='["Motor", "Ultrasound", "RGB LED", "Color Sensor"]'
+enum PingUnit {
+    //% block="cm"
+    Centimeters,
+    //% block="μs"
+    MicroSeconds,
+    //% block="inches"
+    Inches
+}
+enum MotorChannel {
+    //% block="A"
+    MotorA = 1,
+    //% block="B"
+    MotorB = 2,
+    //% block="C"
+    MotorC = 3,
+    //% block="D"
+    MotorD = 4
+}
+enum RGBLedColors {
+    //% block=off
+    Off = 0x000000,
+    //% block=red
+    Red = 0xFF0000,
+    //% block=orange
+    Orange = 0xFFA500,
+    //% block=yellow
+    Yellow = 0xFFFF00,
+    //% block=green
+    Green = 0x00FF00,
+    //% block=blue
+    Blue = 0x0000FF,
+    //% block=indigo
+    Indigo = 0x4b0082,
+    //% block=purple
+    Purple = 0xFF00FF,
+    //% block=white
+    White = 0xFFFFFF
+
+}
+namespace RoboticsWorkshop {
+
+    ////////////////////////////////
+    //          DDM Motor         //
+    ////////////////////////////////
+
+
+    /**馬達通道定義註解
+    A(1,2)
+    B(8,13)
+    C(14,15)
+    D(16,0)
+    I2C(20,19)
     */
-    //% blockId="areaOfRectangle" block="計算X * Y   X %X|Y %Y"
-    //% blockGap=2 weight=0 blockExternalInputs=true
-    export function areaOfRectangle(X: number, Y:number): number {
-        return X*Y
+    //% blockId=DDMmotor2 block="motor channel %MotorPin|speed (0~100) %MSpeedValue|rotation direction(0~1) %McontrolValue" blockExternalInputs=false
+    //% McontrolValue.min=0 McontrolValue.max=1 
+    //% MSpeedValue.min=0 MSpeedValue.max=100   
+    //% group="Motor"
+    export function DDMmotor2(MotorPin: MotorChannel, MSpeedValue: number, McontrolValue: number): void {
+
+        switch (MotorPin) {
+            case 1:
+                pins.analogWritePin(AnalogPin.P1, pins.map(MSpeedValue, 0, 100, 0, 1000));
+                pins.digitalWritePin(DigitalPin.P2, pins.map(McontrolValue, 0, 1, 0, 1));
+                break;
+            case 2:
+                pins.analogWritePin(AnalogPin.P8, pins.map(MSpeedValue, 0, 100, 0, 1000));
+                pins.digitalWritePin(DigitalPin.P13, pins.map(McontrolValue, 0, 1, 0, 1));
+                break;
+            case 3:
+                pins.analogWritePin(AnalogPin.P14, pins.map(MSpeedValue, 0, 100, 0, 1000));
+                pins.digitalWritePin(DigitalPin.P15, pins.map(McontrolValue, 0, 1, 0, 1));
+                break;
+            case 4:
+                pins.analogWritePin(AnalogPin.P16, pins.map(MSpeedValue, 0, 100, 0, 1000));
+                pins.digitalWritePin(DigitalPin.P0, pins.map(McontrolValue, 0, 1, 0, 1));
+                break;
+
+        }
     }
-    /**
-    * 計算長方形面積，不回傳，只顯示在LED
-    */
-    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
-    //% blockGap=2 weight=1
-    export function ledOfRectangle(length: number, width:number): void {
-        basic.showNumber(length*width)
+    /**馬達腳位自行宣告
+      */
+    //% blockId=DDMmotor block="speed pin %MSpeedPin|speed (0~255) %MSpeedValue|direction pin %McontrolPin|rotation direction(0~1) %McontrolValue" blockExternalInputs=false
+    //% McontrolValue.min=0 McontrolValue.max=1 
+    //% MSpeedValue.min=0 MSpeedValue.max=255   
+    //% MSpeedPin.fieldEditor="gridpicker" MSpeedPin.fieldOptions.columns=4
+    //% MSpeedPin.fieldOptions.tooltips="false" MSpeedPin.fieldOptions.width="300"
+    //% McontrolPin.fieldEditor="gridpicker" McontrolPin.fieldOptions.columns=4
+    //% McontrolPin.fieldOptions.tooltips="false" McontrolPin.fieldOptions.width="300"
+    //% group="Motor"
+    export function DDMmotor(MSpeedPin: AnalogPin, MSpeedValue: number, McontrolPin: DigitalPin, McontrolValue: number): void {
+        pins.analogWritePin(MSpeedPin, pins.map(MSpeedValue, 0, 255, 0, 1020));
+        pins.digitalWritePin(McontrolPin, pins.map(McontrolValue, 0, 1, 0, 1));
+
     }
+
+    
+
 }
---
+++ 舊版輩分.ts
@@ -0,0 +1,19 @@
+//% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
+namespace tools {
+    /**
+    * 計算X * Y
+    */
+    //% blockId="areaOfRectangle" block="計算X * Y   X %X|Y %Y"
+    //% blockGap=2 weight=0 blockExternalInputs=true
+    export function areaOfRectangle(X: number, Y:number): number {
+        return X*Y
+    }
+    /**
+    * 計算長方形面積，不回傳，只顯示在LED
+    */
+    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
+    //% blockGap=2 weight=1
+    export function ledOfRectangle(length: number, width:number): void {
+        basic.showNumber(length*width)
+    }
+}
